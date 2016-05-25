8bdc02aa03f81f778c5d8c78d59aa92102b6427f
qinwei_cd <qinwei_cd@huoyunren.com>
2016-05-25 11:30:09 +0800

    aaa

--- src/main/java/util/Lsdfjie.java
+++ src/main/java/util/Lsdfjie.java
@@ -5,7 +5,7 @@ public class Lsdfjie {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(1111);
-		System.out.println(3333);
+		System.out.println("aaaa");
 	}
 	
 	public static void main1(String[] args) {
